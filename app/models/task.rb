d1ead1e090c198fcb948cdd6154a131b43a822e8
shimajiro <saitoua.235@gmail.com>
2023-01-29 09:48:40 +0000

    add column

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :status, presence: true, length: { maximum: 255 }
+  validates :content, presence: true, length: { maximum: 255 }
 end
---
+++ db/migrate/20230129081239_addstatus_totasks.rb
@@ -0,0 +1,4 @@
+class AddstatusTotasks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
