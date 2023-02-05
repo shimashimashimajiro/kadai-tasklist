3694bb1df69b52c39bd56602f2cabebbb2f5c5c8
shimajiro <saitoua.235@gmail.com>
2023-02-05 04:51:31 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,14 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update,:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-      @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+      #@tasks = Task.all
+      #@tasks = User.find(params[:id]) 
+    end
   end
   
   def show
@@ -12,14 +19,17 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
+    
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
       flash.now[:danger] = 'Task が作成されませんでした'
-      render :new
+      #render :new
+      render 'tasks/index'
     end
   end
 
@@ -53,5 +63,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
